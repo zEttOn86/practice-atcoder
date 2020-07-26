817baffbcc2e577f6491fac64615a10c3d0687c9
zEttOn86 <38281934+zEttOn86@users.noreply.github.com>
2020-07-26 14:09:42 +0900

    change to dp solution

--- M-SOLUTIONS-Programming-Contest-2020/D-Road-to-Millionaire.c
+++ M-SOLUTIONS-Programming-Contest-2020/D-Road-to-Millionaire.c
@@ -1,4 +1,14 @@
 #include<stdio.h>
+
+ #define max(a,b) \
+   ({ __typeof__ (a) _a = (a); \
+       __typeof__ (b) _b = (b); \
+     _a > _b ? _a : _b; })
+ #define min(a,b) \
+   ({ __typeof__ (a) _a = (a); \
+       __typeof__ (b) _b = (b); \
+     _a > _b ? _b : _a; })
+
 #define rep(i,n) for(int i=0; i<(n); i++)
 typedef long long ll;
 
@@ -7,17 +17,21 @@ int main()
     int n;
     scanf("%d", &n);
     int a[n];
-    rep(i, n) scanf("%d", a+i);
-    ll x = 1000;
-    rep(i, n-1){
-        int s = a[i];
-        int t = a[i+1];
-        if(s<t){
-            ll k = x/s; // number of stock
-            x %=s;
-            x += k*t;
+    rep(i, n) scanf("%d", a+i+1);
+    ll dp[n+1];
+    dp[0] = 1000; // cashe
+    for(int k=1; k<=n ; k++){ // sell stock day loop
+        dp[k] = 0;
+        for(int i=0; i<k-1; i++){// to decide to buy stock day
+            int m = 99999;
+            for(int j=i+1; j<k; j++){  // get minimum value between k-1 to last selling day
+                m = min(m, a[j]);
+            }
+            dp[k] = max(dp[k], dp[i]/m*a[k]+dp[i]%m);
         }
     }
-    printf("%lld\n", x);
+    ll ans = 0;
+    rep(i, n+1) ans = max(ans, dp[i]);
+    printf("%lld\n", ans);
     return 0;
 }
